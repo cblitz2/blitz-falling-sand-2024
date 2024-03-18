8f0e35b5cf74cd961b34bc98561e0c23130470b3
chanablitz <cblitz2@student.touro.edu>
2024-03-18 14:33:16 -0400

    sand falls

--- src/main/java/blitz/fallingsand/SandComponent.java
+++ src/main/java/blitz/fallingsand/SandComponent.java
@@ -2,7 +2,6 @@ package blitz.fallingsand;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 
 public class SandComponent extends JComponent {
     private final Sand sand;
@@ -15,19 +14,15 @@ public class SandComponent extends JComponent {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        sand.fall();
+        repaint();
+
         for (int y = 0; y < sand.getField().length; y++) {
             for (int x = 0; x < sand.getField()[y].length; x++) {
                 if (sand.get(x, y) == 1) {
-                    g.fillRect(x * 5, y * 5, 5, 5);
+                    g.fillRect(x * 4, y, 5, 5);
                 }
             }
         }
-        int delay = 200;
-        ActionListener animationListener = e -> {
-            sand.fall();
-            repaint();
-        };
-        new Timer(delay, animationListener).start();
     }
-
 }
